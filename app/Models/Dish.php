7170b3fdce51e4d7e8ff11f5c399af669801e479
RixTerreny <riccardotrave43@gmail.com>
2023-03-03 01:46:00 +0100

    model tab ponte added

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Order; 
 
 class Dish extends Model
 {
     use HasFactory;
+
+    public function posts(){
+        return $this->belongsToMany(Order::class);
+    }
 }
