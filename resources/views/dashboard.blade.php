b4abfa5fae880482585c651cff9e83e1b6efd78a
PaoloCrescenzi <crescenzipaolo2@gmail.com>
2023-03-06 18:19:43 +0100

    Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,21 +3,26 @@
 @section('content')
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
+        Benvenuto ... {{--{{ $user->name }}--}}
     </h2>
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header"> <a class="text-decoration-none" href="#">I tuoi piatti</a></div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
+                    <a href=""><button class="btn btn-primary">Aggiungi piatto</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center pt-2 ">
+        <div class="col">
+            <div class="card">
+                <div class="card-header"> <a class="text-decoration-none" href="#">Ordini ricevuti</a></div>
 
-                    {{ __('You are logged in!') }}
+                <div class="card-body">
+                    <a href=""><button class="btn btn-primary">Statistiche ordini</button></a>
                 </div>
             </div>
         </div>
