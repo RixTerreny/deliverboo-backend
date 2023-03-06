cb7e2637d27c2b1d614ec3cc0c2b11bdd7a2d1e5
FilippoBid <FilippoPerrone1997@gmail.com>
2023-03-06 16:15:42 +0100

    modifiche crud registrazione

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Restaurant;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -20,7 +22,9 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        $categories = Category::all();
+        
+        return view('auth.register',compact("categories"));
     }
 
     /**
@@ -30,9 +34,16 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        /* dd(Auth::id()); */
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'lastname' => ['nullable', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],       
+            'photo' => ["file","nullable"],       
+            'user_id' => ["int","required","exists:users,id"],       
+            'restaurant_name' => ['required', 'string', 'max:255'],       
+            'vat' => ['required', 'Numeric', "digits_between:11,11"],
+            'id_category' => ["array","required","exists:categories,id"],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
@@ -43,11 +54,27 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        
+        $restaurant = Restaurant::create([
+            'name' => $request->restaurant_name,
+            'vat' => $request->vat,
+            'address' => $request->address,
+            'user_id' => $user->id,
+            'category' => $request->user->id,
+         
+        ]);
+        if ($request->has("id_category[]")) {
+           
+            $restaurant->categories()->attach($request["id_category[]"]);
+        }
 
         event(new Registered($user));
+      /*   event(new Restaurant($restaurant)); */
+
+        /* event(new Registered($restaurant)); */
 
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect(RouteServiceProvider::HOME, compact($restaurant));
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,6 +37,45 @@
                                 @enderror
                             </div>
                         </div>
+                        <div class="mb-4 row">
+                            <label for="restaurant_name" class="col-md-4 col-form-label text-md-right">{{ __('restaurant name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="restaurant_name" type="text" class="form-control @error('restaurant_name') is-invalid @enderror" name="restaurant_name" value="{{ old('restaurant_name') }}" required autocomplete="restaurant_name" autofocus>
+
+                                @error('restaurant_name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="mb-4 row">
+                            <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
+
+                                @error('address')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="mb-4 row">
+                            <label for="vat" class="col-md-4 col-form-label text-md-right">{{ __('VAT') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="vat" type="text" class="form-control @error('vat') is-invalid @enderror" name="vat" value="{{ old('vat') }}" required autocomplete="vat" autofocus>
+
+                                @error('vat')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="mb-4 row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
@@ -73,7 +112,16 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-
+                        @foreach ($categories as $category)
+                        <div class="form-check form-check-inline @error('id_category') is-invalid @enderror">
+                            {{-- Il name dell'input ha come suffisso le quadre [] che indicheranno al server,
+                            di creare un array con i vari tag che stiamo inviando --}}
+                            <input class="form-check-input @error('id_category') is-invalid @enderror" type="checkbox"
+                                id="technologiesCheckbox_{{ $loop->index }}" value="{{ $category->id }}" name="id_category[]" {{
+                                in_array($category->id, old('id_category', [])) ? 'checked' : '' }}>
+                            <label class="form-check-label" for="technologiesCheckbox_{{ $loop->index }}">{{ $category->name}}</label>
+                        </div>
+                        @endforeach
                         <div class="mb-4 row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -81,6 +129,7 @@
                                 </button>
                             </div>
                         </div>
+                        
                     </form>
                 </div>
             </div>
