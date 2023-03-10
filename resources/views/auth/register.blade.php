3736b2e565ab864ad62971f5a6cf29507a3033a0
RixTerreny <riccardotrave43@gmail.com>
2023-03-10 05:33:44 +0100

    ft 1 added to fr 1, but not at the checkbox

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
 
                             <div class="col-md-6">
                                 <input  id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+                                <span id="err-name"></span>
                                 @error('name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -24,12 +24,13 @@
                                 @enderror
                             </div>
                         </div>
+
                         <div class="mb-4 row">
                             <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('lastname') }}</label>
 
                             <div class="col-md-6">
                                 <input  id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
-
+                                <span id="err-lastname"></span>
                                 @error('lastname')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -37,12 +38,13 @@
                                 @enderror
                             </div>
                         </div>
+
                         <div class="mb-4 row">
                             <label for="restaurant_name" class="col-md-4 col-form-label text-md-right">{{ __('restaurant name') }}</label>
 
                             <div class="col-md-6">
                                 <input  id="restaurant_name" type="text" class="form-control @error('restaurant_name') is-invalid @enderror" name="restaurant_name" value="{{ old('restaurant_name') }}" required autocomplete="restaurant_name" autofocus>
-
+                                <span id="err-rest"></span>
                                 @error('restaurant_name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -50,12 +52,13 @@
                                 @enderror
                             </div>
                         </div>
+
                         <div class="mb-4 row">
                             <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('address') }}</label>
 
                             <div class="col-md-6">
                                 <input  id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
-
+                                <span id="err-address"></span>
                                 @error('address')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -63,12 +66,13 @@
                                 @enderror
                             </div>
                         </div>
+
                         <div class="mb-4 row">
                             <label for="vat" class="col-md-4 col-form-label text-md-right">{{ __('VAT') }}</label>
 
                             <div class="col-md-6">
                                 <input  id="vat" type="text" class="form-control @error('vat') is-invalid @enderror" name="vat" value="{{ old('vat') }}" required autocomplete="vat" autofocus>
-
+                                <span id="err-vat"></span>
                                 @error('vat')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -83,6 +87,7 @@
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
+                                <span id="err-email"></span>
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -97,6 +102,7 @@
                             <div class="col-md-6">
                                 <input  id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
+                                <span id="err-password"></span>
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -110,6 +116,7 @@
 
                             <div class="col-md-6">
                                 <input  id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <span id="err-conf"></span>
                             </div>
                         </div>
 
@@ -138,5 +145,96 @@
             </div>
         </div>
     </div>
+    <script>
+        const password = document.getElementById('password');
+        const err = document.getElementById('err-password')
+        password.addEventListener('input', ()=>{
+            if(password.value.length < 8){
+                err.innerHTML = 'at least 8 character'
+            }
+            else{
+                err.innerHTML = ''
+            }
+        })
+
+        const conf = document.getElementById('password-confirm');
+        const errC = document.getElementById('err-conf')
+        conf.addEventListener('input', ()=>{
+            console.log(password.value);
+            console.log(conf.value);
+            if(conf.value != password.value){
+                errC.innerHTML = 'Passwords do not match'
+            }
+            else{
+                errC.innerHTML = ''
+            }
+        })
+
+        const email = document.getElementById('email');
+        const errP = document.getElementById('err-email')
+        email.addEventListener('input', ()=>{
+            if(email.value.length < 1){
+                errP.innerHTML = 'Required'
+            }
+            else{
+                errP.innerHTML = ''
+            }
+        })
+
+        const vat = document.getElementById('vat');
+        const errV = document.getElementById('err-vat')
+        vat.addEventListener('input', ()=>{
+            if(vat.value.length == 11){
+                errV.innerHTML = 'Insert a valid VAT'
+            }
+            else{
+                errV.innerHTML = ''
+            }
+        })
+
+        const name = document.getElementById('name');
+        const errN = document.getElementById('err-name')
+        name.addEventListener('input', ()=>{
+            if(name.value.length < 1){
+                errN.innerHTML = 'Required'
+            }
+            else{
+                errN.innerHTML = ''
+            }
+        })
+
+        const lastname = document.getElementById('lastname');
+        const errL = document.getElementById('err-lastname')
+        lastname.addEventListener('input', ()=>{
+            if(lastname.value.length < 1){
+                errL.innerHTML = 'Required'
+            }
+            else{
+                errL.innerHTML = ''
+            }
+        })
+
+        const rest = document.getElementById('restaurant_name');
+        const errR = document.getElementById('err-rest')
+        rest.addEventListener('input', ()=>{
+            if(rest.value.length < 1){
+                errR.innerHTML = 'Required'
+            }
+            else{
+                errR.innerHTML = ''
+            }
+        })
+
+        const address = document.getElementById('address');
+        const errA = document.getElementById('err-address')
+        address.addEventListener('input', ()=>{
+            if(address.value.length < 1){
+                errA.innerHTML = 'Required'
+            }
+            else{
+                errA.innerHTML = ''
+            }
+        })
+    </script>
 </div>
 @endsection
