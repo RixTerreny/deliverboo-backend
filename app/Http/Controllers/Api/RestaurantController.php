6bc54989a9f422bdd69b7ccc37824feb3f356cc0
FilippoBid <FilippoPerrone1997@gmail.com>
2023-03-13 11:30:58 +0100

    ripush

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -34,7 +34,7 @@ class RestaurantController extends Controller
 
  */
 
-        $restaurants = Restaurant::whereHas('categories', function($query) use ($category) {
+        $restaurants = Restaurant::whereHas('categories',  function($query) use ($category) {
             $query->where('categories.id', $category);
         })->get();
         
