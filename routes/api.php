1f505c63ca560c1e38a5fbf10a028f59190cdeef
RixTerreny <riccardotrave43@gmail.com>
2023-03-15 18:42:33 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\RestaurantController as ApiRestaurantController;
 use App\Http\Controllers\Api\DishController as ApiDishController;
+use App\Http\Controllers\Api\Orders\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,7 @@ Route::get("/restaurants",[ApiRestaurantController::class,"index"]);
 
 Route::get("/dish/{id}",[ApiDishController::class,"index"]);
 Route::get("/restaurants/category/{category}",[ApiRestaurantController::class,"CategorySerch"]);
+
+// api pagamento
+Route::get('order/generate',[OrderController::class,"generate"]);
+Route::post('order/make/payment',[OrderController::class,"makePayment"]);
